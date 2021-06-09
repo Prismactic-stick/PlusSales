3d9fdcf7737bbe9fe78dffb22dec5b162c8742e9
Ismael Rodriguez <jesus.rodriguez@utacapulco.edu.mx>
2021-06-08 20:43:39 -0500

    Se ha especificado la info del login

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -25,7 +25,7 @@
         </div>
         <!-- /.login-logo -->
         <div class="login-box-body">
-            <p class="login-box-msg">Introduzca sus datos de ingreso</p>
+            <p class="login-box-msg">Introduzca usuario y contraseña</p>
             <?php if($this->session->flashdata("error")):?>
               <div class="alert alert-danger">
                 <p><?php echo $this->session->flashdata("error")?></p>
